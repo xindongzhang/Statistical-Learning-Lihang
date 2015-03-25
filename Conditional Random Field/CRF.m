b619936f5fac5221e268f7aec86dd00a383d4dad
xindong zhang <xindongzhang@Foxmail.com>
2015-03-26 00:42:34 +0800

    CRF

--- Conditional Random Field/CRF.m
+++ Conditional Random Field/CRF.m
@@ -1,15 +1,38 @@
 %% conditional random field %%
 %% 
-function W = CRF(TrainData, TrainLabel, IndexFunc, Label)
+function W = CRF(TrainData, TrainLabel)
 
-if length(TrainData) ~= length(TrainLabel) || ...
-   length(TrainData) ~= length(IndexFunc)  || ...
-   length(IndexFunc) ~= length(TrainLabel) || ...
+if length(TrainData) ~= length(TrainLabel)
     disp(['data and label should be in the same size ']);
     return;
 end
 
+%% -------------- %%
+Label = unique(TrainLabel);
 %% initialize the parameter
-N = length(Label);
-W = zeros(1, N*N);
+NoL = length(Label);
+NoD = length(TrainData);
+W = zeros(1, NoL*NoL);
+IndexFunc = zeros(NoL, NoD);
+
+%% calculate the Index function
+NumLabel = zeros(1, NoL);
+for i = 1:NoD
+    for j = 1:NoL
+        if TrainLabel(i) == Label(j)
+            NumLabel(i) = j;
+            continue;
+        end
+    end
+end
+
+[rows, cols] = size(IndexFunc);
+for i = 1:NoL
+    for j = 1:NoL
+        
+    end
+end
+
+NumLabel
+
 end
--- Conditional Random Field/main.m
+++ Conditional Random Field/main.m
@@ -7,14 +7,10 @@ clc;
 
 
 TrainData = '我爱北京天安门';
-%% index function which is the beginning of a word %%
-IndexFunc = [1,1,1,0,1,0,0];
 TrainLabel = ['S','S','B','E','B','M','E'];
 
-length(TrainData)
+W = CRF(TrainData, TrainLabel);
 
-%% labels of the text labeling
-label = ['B','M','E','S'];
 
 
 
